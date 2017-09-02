4b14a1e41102244b1fc200af1173ee2a33ef3795
liuqi <liuqi0315@gmail.com>
2017-09-02 13:52:51 +0800

    refactor: Rename sqlite test module

--- tests/sqlite.py
+++ tests/sqlite.py
@@ -1,14 +1,14 @@
 import sqlite3
 
-connect = sqlite3.connect('test.db')
+connect = sqlite3.connect('sqlite.db')
 cursor = connect.cursor()
-# cursor.execute('''CREATE TABLE COMPANY(
-#         ID INT PRIMARY KEY     NOT NULL,
-#         NAME           TEXT    NOT NULL,
-#         AGE            INT     NOT NULL,
-#         ADDRESS        CHAR(50),
-#         SALARY         REAL);''')
-# connect.commit()
+cursor.execute('''CREATE TABLE COMPANY(
+        ID INT PRIMARY KEY     NOT NULL,
+        NAME           TEXT    NOT NULL,
+        AGE            INT     NOT NULL,
+        ADDRESS        CHAR(50),
+        SALARY         REAL);''')
+connect.commit()
 
 cursor.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
       VALUES (1, 'Paul', 32, 'California', 20000.00 )");
@@ -23,3 +23,4 @@ cursor.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
       VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
 connect.commit()
 
+connect.close()
