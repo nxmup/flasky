8bfb302d2e5b3838c995f3be2b6e5a4f8ff547dc
liuqi <liuqi0315@gmail.com>
2017-09-02 09:25:32 +0800

    feat: integration Mail

---
+++ ch6.py
@@ -0,0 +1,86 @@
+import os
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "This is a secret key to use CSRF"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <2514980765@qq.com>'
+app.config['FLASKY_ADMIN'] = ["liuqi0315@gmail.com"]
+app.config.update(
+    MAIL_SERVER='smtp.qq.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME='2514980765',
+    MAIL_PASSWORD='zfcxjvmnanmkdjab'
+)
+bootstrap = Bootstrap(app)
+mail = Mail(app)
+db = SQLAlchemy(app)
+
+
+def send_mail(to, subject, template, **kwargs):
+    message = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                      sender=app.config['FLASKY_MAIL_SENDER'],
+                      recipients=to)
+    message.body = render_template(template + '.txt', **kwargs)
+    message.html = render_template(template + '.html', **kwargs)
+    print(message)
+    mail.send(message)
+
+
+class NameForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_mail(app.config['FLASKY_ADMIN'], 'New User',
+                          'mail/new_user', user=user)
+            else:
+                session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                               known=session.get('known', False))
+
+if __name__ == '__main__':
+    app.run(debug=True)
