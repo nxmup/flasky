e6f752c2b590c6616123bf628770262a2d1e877c
liuqi <liuqi0315@gmail.com>
2017-09-08 11:21:21 +0800

    feature: complete Permalink and Editor

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request
 import hashlib
+from markdown import markdown
+import bleach
 
 from . import db
 from . import login_manager
@@ -161,6 +163,8 @@ class Post(db.Model):
     body = db.Column(db.TEXT)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # user post
+    body_html = db.Column(db.TEXT)
 
     @staticmethod
     def generate_fake(count=100):
@@ -177,6 +181,17 @@ class Post(db.Model):
             db.session.add(post)
             db.session.commit()
 
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags,
+            strip=True
+        ))
+
 
 class AnonymouseUser(AnonymousUserMixin):
     def can(self, permissions):
@@ -185,8 +200,9 @@ class AnonymouseUser(AnonymousUserMixin):
     def is_administrator(self):
         return False
 
-login_manager.anonymous_user = AnonymouseUser
 
+login_manager.anonymous_user = AnonymouseUser
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 @login_manager.user_loader
 def load_user(user_id):
