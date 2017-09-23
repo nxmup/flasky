539ac7dfab8fd54966f48e185257beeef6a5cb01
xlui <liuqi0315@gmail.com>
2017-09-23 19:03:32 +0800

    feature: use templates to render page and use bootstrap to enrich templates

--- flasky.py
+++ flasky.py
@@ -1,19 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(debug=True)
+    # manager.run()
