5818ea34a7135ae58e7ff9a70dedf498166b308e
xlui <liuqi0315@gmail.com>
2017-09-23 21:01:19 +0800

    fix: forget to set name

--- flasky.py
+++ flasky.py
@@ -20,12 +20,11 @@ class NameForm(Form):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=name)
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
