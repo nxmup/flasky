43f0abc07d1fe705ea73f0b217766e776a602431
xlui <liuqi0315@gmail.com>
2017-09-24 15:25:26 +0800

    feature: user authentication

---
+++ app/auth/views.py
@@ -0,0 +1,32 @@
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_required, login_user, logout_user
+
+from . import auth
+from .forms import LoginForm
+from ..models import User
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/secret')
+@login_required
+def secret():
+    return '只有登陆用户可以看到本页面！'
---
+++ tests/test_user_model.py
@@ -0,0 +1,27 @@
+import unittest
+from app.models import User
+
+
+class MyTestCase(unittest.TestCase):
+    def test_password_setter(self):
+        user = User(password='cat')
+        self.assertTrue(user.password_hash is not None)
+
+    def test_no_password_getter(self):
+        user = User(password='cat')
+        with self.assertRaises(AttributeError):
+            _ = user.password
+
+    def test_password_verification(self):
+        user = User(password='cat')
+        self.assertTrue(user.verify_password('cat'))
+        self.assertFalse(user.verify_password('dog'))
+
+    def test_password_salts_are_random(self):
+        user1 = User(password='cat')
+        user2 = User(password='cat')
+        self.assertTrue(user1.password_hash != user2.password_hash)
+
+
+if __name__ == '__main__':
+    unittest.main()
