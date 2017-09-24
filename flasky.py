d96f37283f207d4c5d5423a9e5932e526842106e
xlui <liuqi0315@gmail.com>
2017-09-24 10:44:44 +0800

    feature: add database migrate support

--- flasky.py
+++ flasky.py
@@ -7,17 +7,20 @@ from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 
 basedir = os.path.abspath(os.path.dirname(__name__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "hard to guess string"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class NameForm(Form):
@@ -82,4 +85,5 @@ if __name__ == '__main__':
     def make_shell_context():
         return dict(app=app, db=db, User=User, Role=Role)
     manager.add_command('shell', Shell(make_context=make_shell_context))
+    manager.add_command('db', MigrateCommand)
     manager.run()
