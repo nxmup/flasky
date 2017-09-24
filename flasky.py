53f5100f28d8266fa5c5e44ae3c56a511f55db82
xlui <liuqi0315@gmail.com>
2017-09-24 10:28:01 +0800

    feature: operate database in view function

--- flasky.py
+++ flasky.py
@@ -49,12 +49,17 @@ class User(db.Model):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed you name!')
+        new_user = User.query.filter_by(username=form.name.data).first()
+        if new_user is None:
+            new_user = User(username=form.name.data)
+            db.session.add(new_user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 
 @app.route('/user/<name>')
