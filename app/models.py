8b702d0f5de3f7e151180112ae1a6bd4e40e8e7f
xlui <liuqi0315@gmail.com>
2017-09-26 16:03:33 +0800

    feature: generate virtual user and virtual articles

--- app/models.py
+++ app/models.py
@@ -76,6 +76,28 @@ class User(UserMixin, db.Model):
     # posts
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+
+        seed()
+        for i in range(count):
+            user = User(email=forgery_py.internet.email_address(),
+                        username=forgery_py.internet.user_name(True),
+                        password=forgery_py.lorem_ipsum.word(),
+                        confirmed=True,
+                        name=forgery_py.name.full_name(),
+                        location=forgery_py.address.city(),
+                        about_me=forgery_py.lorem_ipsum.sentence(),
+                        member_since=forgery_py.date.date(True))
+            db.session.add(user)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -141,6 +163,21 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed, randint
+        import forgery_py
+
+        seed()
+        user_count = User.query.count()
+        for i in range(count):
+            user = User.query.offset(randint(0, user_count - 1)).first()
+            post = Post(body=forgery_py.lorem_ipsum.sentences(randint(1, 3)),
+                        timestamp=forgery_py.date.date(True),
+                        author=user)
+            db.session.add(post)
+            db.session.commit()
 
 
 @login_manager.user_loader
